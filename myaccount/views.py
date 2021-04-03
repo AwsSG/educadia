92f5ba2f7e44843e1c357bdf11bf37b53ce96355
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-03 22:21:02 +0000

    updated myaccount template view

--- myaccount/views.py
+++ myaccount/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserAccount
 # Create your views here.
 
 
 def myaccount(request):
     """ Display the user's account """
+    account = get_object_or_404(UserAccount, user=request.user)
 
     template = 'myaccount/myaccount.html'
-    context = {}
+    context = {
+        'account': account,
+    }
 
     return render(request, template, context)
