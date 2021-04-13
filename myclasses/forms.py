079266d4643766e7af81c57a7604b8f23e01c795
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-13 19:08:55 +0000

    creatd AllClasses model and added it in forms.py

---
+++ myclasses/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import AllClasses
+
+
+class AllClassesForm(forms.ModelForm):
+    class Meta:
+        model = AllClasses
+        fields = ['class_name',
+                  'class_subject',
+                  'class_university',
+                  'class_collage',
+                  'class_department',
+                  'class_level',
+                  'class_division',
+                  'class_year']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'class_name': 'Title',
+            'class_subject': 'Subject',
+            'class_university': 'University',
+            'class_collage': 'Collage',
+            'class_department': 'Department',
+            'class_level': 'Level',
+            'class_division': 'Division',
+            'class_year': 'Year',
+        }
+
+        self.fields['class_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-control f-input'
+            self.fields[field].label = False
--- myclasses/models.py
+++ myclasses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from myaccount.models import UserAccount
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class AllClasses(models.Model):
+    """
+    The main classes model, contains all the availale classes
+    """
+    added_by = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    class_name = models.CharField(max_length=100, null=False, blank=False)
+    class_subject = models.CharField(max_length=100, null=False, blank=False)
+    class_university = models.CharField(max_length=100, null=False, blank=False)
+    class_collage = models.CharField(max_length=100, null=False, blank=False)
+    class_department = models.CharField(max_length=100, null=False, blank=False)
+    class_level = models.CharField(max_length=100, null=False, blank=False)
+    class_division = models.CharField(max_length=100, null=False, blank=False)
+    class_year = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.class_name or ''
+
+
+@receiver(post_save, sender=User)
+def create_or_update_class(sender, instance, created, **kwargs):
+    """
+    Create or update the class
+    """
+    if created:
+        AllClasses.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.allclasses.save()
