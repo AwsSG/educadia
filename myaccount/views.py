4d71ffca6c3c7e7edd7ab41b7d0e2585da47ef19
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-13 17:22:22 +0000

    added authentication checks to myaccout and myclasses templates

--- myaccount/views.py
+++ myaccount/views.py
@@ -7,20 +7,22 @@ from django.contrib import messages
 
 def myaccount(request):
     """ Display the user's account """
-    account = get_object_or_404(UserAccount, user=request.user)
+    if request.user.is_authenticated:
+        account = get_object_or_404(UserAccount, user=request.user)
+        if request.method == 'POST':
+            form = UserAccountForm(request.POST, instance=account)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Profile updated successfully')
 
-    if request.method == 'POST':
-        form = UserAccountForm(request.POST, instance=account)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
+        form = UserAccountForm(instance=account)
 
-    form = UserAccountForm(instance=account)
-
-    template = 'myaccount/myaccount.html'
-    context = {
-        'account': account,
-        'form': form,
-    }
-
-    return render(request, template, context)
+        template = 'myaccount/myaccount.html'
+        context = {
+            'account': account,
+            'form': form,
+        }
+        return render(request, template, context)
+    else:
+        template = 'myaccount/myaccount.html'
+        return render(request, template)
