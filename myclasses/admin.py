50a094a13e3214528a4c8cc1982b287d7c6c581b
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-15 00:52:20 +0000

    added AllClasses to admin and fixed POST handler

--- myclasses/admin.py
+++ myclasses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import AllClasses
 # Register your models here.
+
+
+class MyClassesAdmin(admin.ModelAdmin):
+    list_display = (
+        'class_name',
+        'added_by',
+        'class_join_code',
+        'class_subject',
+        'class_university',
+        'class_collage',
+        'class_department',
+        'class_level',
+        'class_division',
+        'class_year'
+    )
+
+
+admin.site.register(AllClasses, MyClassesAdmin)
--- myclasses/views.py
+++ myclasses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import AllClasses
+from myaccount.models import UserAccount
 from .forms import AllClassesForm
 from django.contrib import messages
 
@@ -9,15 +10,22 @@ from django.contrib import messages
 def myclasses(request):
     """ Creates new classes if the user is authenticated as 'teacher' """
     if request.user.is_authenticated:
-        form = AllClassesForm()
+        teacher = get_object_or_404(UserAccount, user=request.user)
         if request.method == 'POST':
+            form = AllClassesForm(request.POST)
             if form.is_valid():
-                form.save()
+                new_class = form.save(commit=False)
+                new_class.added_by = teacher
+                new_class.save()
                 messages.success(request, 'Class added successfully')
+        else:
+            form = AllClassesForm()
+
         template = 'myclasses/myclasses.html'
         context = {
             'form': form,
         }
+
         return render(request, template, context)
     else:
         template = 'myclasses/myclasses.html'
