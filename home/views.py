31a3bf89ab01e165289d43e87c453faa9bfd4c89
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-17 16:39:27 +0000

    added classes and teachers to home view

--- home/views.py
+++ home/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from myclasses.models import AllClasses
+from myaccount.models import UserAccount
 
 # Create your views here.
 
 
 def index(request):
     """ A view to show the home page """
+    teachers = UserAccount.objects.filter(user_type='teacher')
+    classes = AllClasses.objects.all()
+    context = {
+        'teachers': teachers,
+        'classes': classes,
+    }
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
