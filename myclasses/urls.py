4b54dc5ecc3c19b8b24a7b9a57e41c050b2228cf
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-20 04:13:48 +0000

    added style to materials form and int to class_id

--- myclasses/urls.py
+++ myclasses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.myclasses, name='myclasses'),
-    path('<class_id>', views.class_detail, name='class_detail'),
+    path('<int:class_id>', views.class_detail, name='class_detail'),
 ]
