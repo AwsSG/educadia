de88aeb2da5cf270582c803bce12be6215328b06
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-26 18:51:45 +0000

    created donate app and model

---
+++ donate/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Donate(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def __str__(self):
+        return self.name
--- educadia/settings.py
+++ educadia/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'myaccount',
     'myclasses',
+    'donate',
 
     # other
     'crispy_forms',
