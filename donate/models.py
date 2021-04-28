7464666560970749a09bbbfc03fd6101f98fe408
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-28 19:05:50 +0000

    added basic donate functionality

--- donate/models.py
+++ donate/models.py
@@ -5,7 +5,7 @@ class Donate(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=25)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- donate/views.py
+++ donate/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import DonateForm
+from django.conf import settings
+import stripe
 
 
 def donate(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     donate_form = DonateForm()
+    donated_amount = donate_form['amount'].value()
+    stripe_total = stripe_total = round(donated_amount * 100)
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    print(donated_amount)
+    print(intent)
+
     context = {
         'donate_form': donate_form,
-        'stripe_public_key':
-        'pk_test_51IkYXvIT9OmxcWEwyhVrcIDxF3KoC2xFLKc7nclICOdiZgRIpjAMhiMvAnfAiRlwNZDnPlegDXWoQGTkxJnYQ0ae00ofJ7siq9',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'donate/donate.html', context)
