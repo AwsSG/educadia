9bee2aa4b34bf82cefa7b371bff65b5ec4f6a340
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-28 03:46:52 +0000

    added stripe elements

--- donate/views.py
+++ donate/views.py
@@ -5,7 +5,10 @@ from .forms import DonateForm
 def donate(request):
     donate_form = DonateForm()
     context = {
-        'donate_form': donate_form
+        'donate_form': donate_form,
+        'stripe_public_key':
+        'pk_test_51IkYXvIT9OmxcWEwyhVrcIDxF3KoC2xFLKc7nclICOdiZgRIpjAMhiMvAnfAiRlwNZDnPlegDXWoQGTkxJnYQ0ae00ofJ7siq9',
+        'client_secret': 'test client secret'
     }
 
     return render(request, 'donate/donate.html', context)
