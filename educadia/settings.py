54ae350e7c26d1a3efc8a5b1337096684024bffb
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-30 02:48:21 +0000

    removed secret key and set debug

--- educadia/settings.py
+++ educadia/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2p3^2!u!5=4v#)wm&xc04g0#)(mdlbekj)*oo%n3+g#f5_d7w8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['educadia-test.herokuapp.com', 'localhost']
 
