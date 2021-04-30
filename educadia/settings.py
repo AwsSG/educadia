ae68bb8d976dfdb99454827034d45948fb9805ea
AwsSG <aws.sabah.gheni@gmail.com>
2021-04-30 02:18:48 +0000

    deploying to heroku

--- educadia/settings.py
+++ educadia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2p3^2!u!5=4v#)wm&xc04g0#)(mdlbekj)*oo%n3+g#f5_d7w8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['educadia-test.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,19 @@ WSGI_APPLICATION = 'educadia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
