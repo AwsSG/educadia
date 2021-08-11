84f771dab4d89fb5b6f554f17c6df8ed6a47974c
AwsSG <aws.sabah.gheni@gmail.com>
2021-08-11 15:17:09 +0000

    set stripe up webhook listener

--- donate/urls.py
+++ donate/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.donate, name="donate"),
+    path('wh/', webhook, name='webhook'),
 ]
