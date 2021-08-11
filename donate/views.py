d1e189ee447c2972da91495b6bb4c84d296afe03
AwsSG <aws.sabah.gheni@gmail.com>
2021-08-11 00:00:37 +0000

    added donate post handler

--- donate/views.py
+++ donate/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import DonateForm
 from django.conf import settings
@@ -9,22 +9,31 @@ def donate(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    donate_form = DonateForm()
-    donated_amount = donate_form['amount'].value()
-    stripe_total = stripe_total = round(donated_amount * 100)
+    if request.method == 'POST':
+        donate_form = DonateForm(request.POST)
+        if donate_form.is_valid():
+            donate_form.save()
+            messages.success(request, 'Thank you for your donation!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Form incorrect, make sure you fill the required fields correctly')
+    else:
+        donate_form = DonateForm()
+        donated_amount = donate_form['amount'].value()
+        stripe_total = stripe_total = round(donated_amount * 100)
 
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
-    print(donated_amount)
-    print(intent)
+        # print(donated_amount)
+        # print(intent)
 
     context = {
         'donate_form': donate_form,
