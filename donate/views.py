999c49236c1635430935af692aaff9c92af94baf
AwsSG <aws.sabah.gheni@gmail.com>
2021-08-12 12:57:43 +0000

    added payment intent inside POST block

--- donate/views.py
+++ donate/views.py
@@ -8,25 +8,29 @@ import stripe
 def donate(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_total = 1 * 100
 
     if request.method == 'POST':
         donate_form = DonateForm(request.POST)
         if donate_form.is_valid():
+            donated_amount = donate_form['amount'].value()
+            stripe_total = round(float(donated_amount) * 100)
             donate_form.save()
+            intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )
             messages.success(request, 'Thank you for your donation!')
             return redirect(reverse('home'))
         else:
             messages.error(request, 'Form incorrect, make sure you fill the required fields correctly')
     else:
         donate_form = DonateForm()
-        donated_amount = donate_form['amount'].value()
-        stripe_total = stripe_total = round(donated_amount * 100)
-
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
-        )
+            )
 
         if not stripe_public_key:
             messages.warning(request, 'Stripe public key is missing. \
